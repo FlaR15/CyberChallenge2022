4325cdd97703d77d59507928c3fb09b63ed63b3b
Flavia <101348965+FlaR15@users.noreply.github.com>
2026-03-06 12:52:17 +0100

    Delete cryptic whispers directory

--- cryptic whispers/extract.sh
+++
@@ -1,8 +0,0 @@
-in=cattura.pcapng
-out=dumps/dump
-[ -d 'dumps' ] || mkdir dumps
-for stream in $(tshark -nlr $in -Y tcp.flags.syn==1 -T fields -e tcp.stream | sort -n | uniq | sed 's/\r//')
-do
-        echo "Stream $stream: ${out}_${stream}"
-            tshark -nlr $in -qz "follow,tcp,raw,$stream" | tail -n +7 | sed 's/^\s\+//g' | xxd -r -p > ${out}_${stream}
-done
